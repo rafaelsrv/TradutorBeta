260e24ee10931d29282341c19805be486418b7d9
RafaelSRV <rafael-srv@hotmail.com>
2024-11-01 16:10:49 -0300

    Subindo Projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,93 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+import { languages } from "./languages";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [translated, setTranslated] = useState("");
+  const [textToTranslate, setTextToTranslate] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState("en-GB");
+  const [selectedLanguageToTranslate, setSelectedLanguageToTranslate] = useState("pt-PT")
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const url = `https://api.mymemory.translated.net/get?q=${textToTranslate}&langpair=${selectedLanguage}|${selectedLanguageToTranslate}`;
+
+  // Função para buscar a tradução ao clicar no botão
+  async function buscar() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setTranslated(data.responseData.translatedText);
+    } catch (error) {
+      console.log('Erro ao buscar dados', error);
+    }
+  }
+
+  const changeLanguage = () =>{
+    const temp = selectedLanguage;
+    setSelectedLanguage(selectedLanguageToTranslate);
+    setSelectedLanguageToTranslate(temp)
+  }
+
+ 
+  const handleTextChange = (e) => {
+    setTextToTranslate(e.target.value);
+  };
+
+  const handleLanguageKey = (e) => {
+    setSelectedLanguage(e.target.value)
+  }
+  const handleSelectLanguageToTranslate = (e) => {
+    setSelectedLanguageToTranslate(e.target.value)
+  }
+  
+
+  return (
+    <div className="h-screen w-screen bg-blue-200">
+      <div className="h-full w-full flex flex-col justify-center items-center">
+        <div className="flex justify-center items-center">
+          <div className="flex flex-col m-10">
+            <textarea
+              onChange={handleTextChange}
+              value={textToTranslate}
+              className="w-96 h-72 text-2xl bg-red-400 resize-none"
+              placeholder="Digite o texto para traduzir"
+            ></textarea>
+            <select value={selectedLanguage} onChange={handleLanguageKey} className="mt-2">
+              {Object.entries(languages).map(([key, name])=>
+                <option  key={key} value={key}>{name}</option>
+              )}
+              
+            </select>
+          </div>
+
+          <div className="flex flex-col justify-center m-10">
+            <textarea
+              value={translated}
+              readOnly
+              className="w-96 h-72 bg-red-400 resize-none"
+              placeholder="Tradução aparecerá aqui"
+            ></textarea>
+            <select value={selectedLanguageToTranslate} onChange={handleSelectLanguageToTranslate} className="mt-2 ">
+              {Object.entries(languages).map(([key, name])=>
+                <option key={key} value={key}>{name}</option>
+              )}
+              
+            </select>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={changeLanguage}  // Chama a função de tradução ao clicar no botão
+          className="bg-yellow-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-yellow-600 transition duration-300 ease-in-out transform hover:scale-105"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Trocar
+        </button>
+        <button
+          onClick={buscar}  // Chama a função de tradução ao clicar no botão
+          className="bg-blue-500 mt-2 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Traduzir
+        </button>
+        
+      </div>
     </div>
   );
 }
