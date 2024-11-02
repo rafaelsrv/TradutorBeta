bd6ca444548193285a6945b27428597b091e5f82
RafaelSRV <rafael-srv@hotmail.com>
2024-11-01 23:27:59 -0300

    Adicionando histórico

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { languages } from "./languages";
 
 export default function Home() {
@@ -7,19 +7,30 @@ export default function Home() {
   const [textToTranslate, setTextToTranslate] = useState("");
   const [selectedLanguage, setSelectedLanguage] = useState("en-GB");
   const [selectedLanguageToTranslate, setSelectedLanguageToTranslate] = useState("pt-PT")
+  const [completedHistory, setCompleteHistory] = useState([])
+  let completeHistory = []
 
   const url = `https://api.mymemory.translated.net/get?q=${textToTranslate}&langpair=${selectedLanguage}|${selectedLanguageToTranslate}`;
 
   // Função para buscar a tradução ao clicar no botão
-  async function buscar() {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setTranslated(data.responseData.translatedText);
-    } catch (error) {
-      console.log('Erro ao buscar dados', error);
+
+  let dadosHistoricos = []
+  
+
+    async function buscar() {
+      if(textToTranslate.length>0){ 
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          setTranslated(data.responseData.translatedText);
+        } catch (error) {
+          console.log('Erro ao buscar dados', error);
+        }
+      }
+      
     }
-  }
+ 
+  
 
   const changeLanguage = () =>{
     const temp = selectedLanguage;
@@ -38,11 +49,45 @@ export default function Home() {
   const handleSelectLanguageToTranslate = (e) => {
     setSelectedLanguageToTranslate(e.target.value)
   }
+  const deleteStorage = () => {
+    localStorage.clear()
+    setCompleteHistory([])
+    
+   
+    
+  }
+
+  if (typeof window !== "undefined") {
+    // Aqui, o localStorage estará disponível
+    localStorage.setItem('key', 'value');
+  }
+ 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      
+        const historico = JSON.parse(localStorage.getItem('historico')) || [];
+        if(textToTranslate.length>0) {
+      historico.push({ original: textToTranslate, translated, date: new Date().toLocaleString() });
+        }
+      localStorage.setItem('historico', JSON.stringify(historico));
+      historico.map((data) =>completeHistory.push(data.original))
+      setCompleteHistory(completeHistory)
+      
+      
+      
+      
+           
+    }
+  }, [translated]);
+
+ 
+  console.log(completedHistory)
+  
   
 
   return (
-    <div className="h-screen w-screen bg-blue-200">
-      <div className="h-full w-full flex flex-col justify-center items-center">
+    <div className=" h-full min-h-screen w-full bg-blue-200">
+      <div className=" flex flex-col justify-center items-center">
         <div className="flex justify-center items-center">
           <div className="flex flex-col m-10">
             <textarea
@@ -86,6 +131,27 @@ export default function Home() {
         >
           Traduzir
         </button>
+        <button
+          onClick={deleteStorage}  // Chama a função de tradução ao clicar no botão
+          className="bg-red-400 mt-2 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-red-600 transition duration-300 ease-in-out transform hover:scale-105"
+        >
+          Deletar palavras salvas
+        </button>
+        
+        
+        <div>
+          <div className="grid grid-cols-4	">
+          {completedHistory.map((item, index) =>
+              <div key={index} className="w-32 h-32 bg-slate-600 m-5">
+                {item}
+              </div>
+            )}
+          </div>
+          
+            
+                          
+        </div>
+        
         
       </div>
     </div>
